8b1124ed7a101d60baeee4503f70995ed5a7f685
MarwaAwd <marwaawd5@gmail.com>
2023-05-09 23:30:10 +0200

    ربنا ستر

--- src/ML1/Octree.java
+++ src/ML1/Octree.java
@@ -1,6 +1,8 @@
 package ML1;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
 import java.util.Vector;
 
 public class Octree {
@@ -8,11 +10,17 @@ public class Octree {
     private Vector<Point> points = new Vector<>();
     private Point topLeftFront;
     private Point bottomRightBack;
-
+    private static int maxEntries = Integer.parseInt(getVal("MaximumEntriesinOctreeNode"));
 
     public Octree(){
 
     }
+
+    //          r
+    //   c c c c c c c c
+    // 8  8  8  8 8 8 8 8
+    // if points size < max insert
+
 
     public Octree(Comparable xmin, Comparable ymin, Comparable zmin, Comparable xmax, Comparable ymax, Comparable zmax){
         topLeftFront = new Point(xmin, ymin, zmin);
@@ -37,6 +45,80 @@ public class Octree {
 
     public boolean isLeaf(){
         return bbs.isEmpty();
+    }
+
+    public void insert(Comparable x, Comparable y, Comparable z, int pageNum) throws DBAppException {
+        if(x.compareTo(topLeftFront.x)<0  || x.compareTo(bottomRightBack.x)>0 ||
+                y.compareTo(topLeftFront.y)<0  || y.compareTo(bottomRightBack.y)>0 ||
+                z.compareTo(topLeftFront.z)<0  || z.compareTo(bottomRightBack.z)>0){
+            throw new DBAppException("Out of range");
+        }
+        if(points.size()<maxEntries){
+            points.add(new Point(x,y,z, pageNum));
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        printMiddleString("abxy", "cd");
+    }
+
+    static void printMiddleString(String S, String T){
+        int N = S.length();
+        if(T.length()>S.length()){
+            S += T.substring(S.length());
+            N = T.length();
+        }else if(T.length()<S.length()){
+            T += S.substring(T.length());
+        }
+        // Stores the base 26 digits after addition
+        int[] a1 = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            a1[i + 1] = (int)S.charAt(i) - 97
+                    + (int)T.charAt(i) - 97;
+        }
+        // Iterate from right to left
+        // and add carry to next position
+        for (int i = N; i >= 1; i--) {
+            a1[i - 1] += (int)a1[i] / 26;
+            a1[i] %= 26;
+        }
+        // Reduce the number to find the middle
+        // string by dividing each position by 2
+        for (int i = 0; i <= N; i++) {
+            // If current value is odd,
+            // carry 26 to the next index value
+            if ((a1[i] & 1) != 0) {
+
+                if (i + 1 <= N) {
+                    a1[i + 1] += 26;
+                }
+            }
+            a1[i] = (int)a1[i] / 2;
+        }
+        for (int i = 1; i <= N; i++) {
+            System.out.print((char)(a1[i] + 97));
+        }
+    }
+
+    public static String getVal(String key) {
+        String keyval = null;
+        //Let's consider properties file is in project folder itself
+
+        File file = new File("src/Resources/DBApp.config");
+
+        //Creating properties object
+        Properties prop = new Properties();
+        //Creating InputStream object to read data
+        FileInputStream objInput = null;
+        try{
+            objInput = new FileInputStream(file);
+            //Reading properties key/values in file
+            prop.load(objInput);
+            keyval = prop.getProperty(key);
+            objInput.close();
+        }catch(Exception e){System.out.println(e.getMessage());}
+        return keyval;
     }
 
 }
